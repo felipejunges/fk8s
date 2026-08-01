749ce12f3f0b97d5c2c7c52679d8256c3c89122a
Felipe Junges <felipejunges@yahoo.com.br>
2026-08-01 12:28:29 -0300

    v6

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ app.MapGet("/weatherforecast", () =>
                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                     Random.Shared.Next(-20, 55),
                     summaries[Random.Shared.Next(summaries.Length)],
-                    Version: "v4"
+                    Version: "v6"
                 ))
             .ToArray();
         return forecast;
